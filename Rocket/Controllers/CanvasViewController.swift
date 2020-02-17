c5d4bc15f68204581afae23a5abda886e107b156
Kutan <cingisiz.akutan@gmail.com>
2020-02-17 09:08:05 +0300

    Remove useless storyboard

---
+++ Rocket/Controllers/CanvasViewController.swift
@@ -0,0 +1,17 @@
+//
+//  CanvasViewController.swift
+//  Rocket
+//
+//  Created by KUTAN ÇINGISIZ on 17.02.2020.
+//  Copyright © 2020 KUTAN ÇINGISIZ. All rights reserved.
+//
+
+import UIKit
+
+class CanvasViewController: UIViewController {
+    
+    override func loadView() {
+        view = UIView()
+        view.backgroundColor = .white
+    }
+}
